92cb8317f222b3bc9a13f9d6493334f5579133b8
Gary Golden <mail@garygolden.me>
2011-05-22 22:27:35 +0600

    [admin Page] generated module to manage Pages

---
+++ apps/admin/modules/pages/lib/pagesGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * pages module helper.
+ *
+ * @package    anonvpn.com
+ * @subpackage pages
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pagesGeneratorHelper extends BasePagesGeneratorHelper
+{
+}

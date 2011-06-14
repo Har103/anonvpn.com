5ffdde608f7374352b5670f11f8bd4747b8c384d
Gary Golden <mail@garygolden.me>
2011-06-15 02:10:30 +0600

    _weAccept partial

---
+++ apps/frontend/templates/_weAccept.php
@@ -0,0 +1,10 @@
+<h2>
+	<span>We accept</span>
+</h2>
+<a href="#">
+	<img src="/images/gallery_1.jpg" width="58" height="58" alt="pix" />
+</a>
+<a href="#">
+	<img src="/images/gallery_2.jpg" width="58" height="58" alt="pix" />
+</a>
+<a href="#"><img src="/images/gallery_3.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_4.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_5.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_6.jpg" width="58" height="58" alt="pix" /></a>

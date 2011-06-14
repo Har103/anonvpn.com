7aa12b503d06b71143c76d537f8868af97c8263f
Gary Golden <mail@garygolden.me>
2011-06-14 23:50:02 +0600

    [Blogpost] model

---
+++ lib/filter/doctrine/BlogpostFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Blogpost filter form.
+ *
+ * @package    anonvpn.com
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BlogpostFormFilter extends BaseBlogpostFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/BlogpostForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Blogpost form.
+ *
+ * @package    anonvpn.com
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BlogpostForm extends BaseBlogpostForm
+{
+  public function configure()
+  {
+  }
+}

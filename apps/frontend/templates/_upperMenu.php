78828ea21a51edc8fcd64ab1a868fa3ede1b0b0d
Gary Golden <mail@garygolden.me>
2011-06-14 22:06:44 +0600

    move upper menu into partial

---
+++ apps/frontend/templates/_upperMenu.php
@@ -0,0 +1,10 @@
+<div class="menu_nav">
+ 	<ul>
+  	<li class="active"><a href="index.html"><span>Home</span></a></li>
+    <li><a href="support.html"><span>Support</span></a></li>
+    <li><a href="about.html"><span>About Us</span></a></li>
+    <li><a href="blog.html"><span>Blog</span></a></li>
+    <li><a href="contact.html"><span>Contact Us</span></a></li>
+	</ul>
+  <div class="clr"></div>
+</div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -14,18 +14,9 @@
       <div class="header">
         <div class="header_resize">
 
-					<?php include_partial('global/logo'); ?>
+					<?php include_partial('global/logo')?>
+					<?php include_partial('global/upperMenu')?>
 
-          <div class="menu_nav">
-           <ul>
-            <li class="active"><a href="index.html"><span>Home</span></a></li>
-              <li><a href="support.html"><span>Support</span></a></li>
-              <li><a href="about.html"><span>About Us</span></a></li>
-              <li><a href="blog.html"><span>Blog</span></a></li>
-              <li><a href="contact.html"><span>Contact Us</span></a></li>
-          </ul>
-            <div class="clr"></div>
-          </div>
           <div class="clr"></div>
           <div class="header_img"><img src="images/main_img.png" alt="image" width="271" height="234" />
           <h2>best proposition</h2>

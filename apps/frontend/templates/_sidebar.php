7c02ef8978245ecc40f5ad914ab2d274b9fe1b78
Gary Golden <mail@garygolden.me>
2011-06-15 02:10:12 +0600

    _sidebar partial

---
+++ apps/frontend/templates/_sidebar.php
@@ -0,0 +1,39 @@
+<div class="sidebar">
+	<?php // include_partial('global/search')?>
+  <div class="gadget">
+    <h2>Sidebar Menu</h2>
+    <div class="clr"></div>
+    <ul class="sb_menu">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">TemplateInfo</a></li>
+      <li><a href="#">Style Demo</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Archives</a></li>
+      <li><a href="http://www.dreamtemplate.com/">Web Templates</a></li>
+    </ul>
+  </div>
+  <div class="gadget">
+    <h2><span>Sponsors</span></h2>
+    <div class="clr"></div>
+    <ul class="ex_menu">
+      <li><a href="http://www.dreamtemplate.com" title="Website Templates">DreamTemplate</a><br />
+        Over 6,000+ Premium Web Templates</li>
+      <li><a href="http://www.templatesold.com" title="WordPress Themes">TemplateSOLD</a><br />
+        Premium WordPress &amp; Joomla Themes</li>
+      <li><a href="http://www.imhosted.com" title="Affordable Hosting">ImHosted.com</a><br />
+        Affordable Web Hosting Provider</li>
+      <li><a href="http://www.myvectorstore.com" title="Stock Icons">MyVectorStore</a><br />
+        Royalty Free Stock Icons</li>
+      <li><a href="http://www.evrsoft.com" title="Website Builder">Evrsoft</a><br />
+        Website Builder Software &amp; Tools</li>
+      <li><a href="http://www.csshub.com/" title="CSS Templates">CSS Hub</a><br />
+        Premium CSS Templates</li>
+    </ul>
+  </div>
+  <div class="gadget">
+    <h2>Wise Words</h2>
+    <div class="clr"></div>
+    <p>   <img src="/images/test_1.gif" alt="image" width="19" height="20" /> <em>We can let circumstances rule us, or we can take charge and rule our lives from within </em>.<img src="/images/test_2.gif" alt="image" width="19" height="20" /></p>
+    <p style="float:right;"><strong>Earl Nightingale</strong></p>
+  </div>
+</div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -30,57 +30,17 @@
           <div class="mainbar">
 						<?php echo $sf_content; ?>
           </div>
-          <div class="sidebar">
-						<?php // include_partial('global/search')?>
-            <div class="gadget">
-              <h2>Sidebar Menu</h2>
-              <div class="clr"></div>
-              <ul class="sb_menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">TemplateInfo</a></li>
-                <li><a href="#">Style Demo</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Archives</a></li>
-                <li><a href="http://www.dreamtemplate.com/">Web Templates</a></li>
-              </ul>
-            </div>
-            <div class="gadget">
-              <h2><span>Sponsors</span></h2>
-              <div class="clr"></div>
-              <ul class="ex_menu">
-                <li><a href="http://www.dreamtemplate.com" title="Website Templates">DreamTemplate</a><br />
-                  Over 6,000+ Premium Web Templates</li>
-                <li><a href="http://www.templatesold.com" title="WordPress Themes">TemplateSOLD</a><br />
-                  Premium WordPress &amp; Joomla Themes</li>
-                <li><a href="http://www.imhosted.com" title="Affordable Hosting">ImHosted.com</a><br />
-                  Affordable Web Hosting Provider</li>
-                <li><a href="http://www.myvectorstore.com" title="Stock Icons">MyVectorStore</a><br />
-                  Royalty Free Stock Icons</li>
-                <li><a href="http://www.evrsoft.com" title="Website Builder">Evrsoft</a><br />
-                  Website Builder Software &amp; Tools</li>
-                <li><a href="http://www.csshub.com/" title="CSS Templates">CSS Hub</a><br />
-                  Premium CSS Templates</li>
-              </ul>
-            </div>
-            <div class="gadget">
-              <h2>Wise Words</h2>
-              <div class="clr"></div>
-              <p>   <img src="/images/test_1.gif" alt="image" width="19" height="20" /> <em>We can let circumstances rule us, or we can take charge and rule our lives from within </em>.<img src="/images/test_2.gif" alt="image" width="19" height="20" /></p>
-              <p style="float:right;"><strong>Earl Nightingale</strong></p>
-              </div>
-                 </div>
+          <?php // include_partial('global/sidebar')?>
           <div class="clr"></div>
         </div>
       </div>
       <div class="fbg">
         <div class="fbg_resize">
           <div class="col c1">
-            <h2><span>Image Gallery</span></h2>
-            <a href="#"><img src="/images/gallery_1.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_2.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_3.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_4.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_5.jpg" width="58" height="58" alt="pix" /></a> <a href="#"><img src="/images/gallery_6.jpg" width="58" height="58" alt="pix" /></a> </div>
+          	<?php include_partial('global/weAccept')?>
+          </div>
           <div class="col c2">
-            <h2><span>Lorem Ipsum</span></h2>
-            <p>Lorem ipsum dolor<br />
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. Cras id urna. <a href="#">Morbi tincidunt, orci ac convallis aliquam</a>, lectus turpis varius lorem, eu posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum odio, ac blandit ante orci ut diam.</p>
+          	<?php include_partial('global/ourTechnologies')?>
           </div>
           <div class="col c3">
             <h2><span>Contact</span></h2>

2ee21b8a7c4a0c90b8fefc7aa655ba6b35fb1233
Gary Golden <mail@garygolden.me>
2011-06-15 03:20:30 +0600

    _basicPackage partial

---
+++ apps/frontend/modules/static/templates/_basicPackage.php
@@ -0,0 +1,26 @@
+<div class="basic-package">
+  <h2>Basic VPN service</h2>
+  <div class="clr"></div>
+  <p>Standard set of features suitable for most users.</p>
+	<ul>
+		<li>Instant activation</li>
+		<li>Web control panel</li>
+		<li>Encrypted VPN using PPTP</li>
+		<li>Access to VPN servers in Europe</li>
+		<li>50GB of transfer</li>
+		<li>Technical support</li>
+		<li>7-day money back guarantee</li>
+	</ul>
+	<div class="price">$7</div>
+	<div class="button">
+  	<form action='https://www.2checkout.com/checkout/purchase' method='post'>
+      <input type='hidden' name='sid' value='1507059' />
+      <input type='hidden' name='quantity' value='1' />
+      <input type='hidden' name='product_id' value='1' />
+      <?php if ($sf_context->getConfiguration()->getEnvironment() === 'dev') : ?>
+      <input type='hidden' name='demo' value='Y' />
+      <?php endif; ?>
+      <input class="button" name='submit' type='submit' value='Purchase for $7' />
+  	</form>
+	</div>
+</div>

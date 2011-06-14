033c50d6d58b36ff41cc8e30bd1fd56b5acebca4
Gary Golden <mail@garygolden.me>
2011-06-14 22:49:52 +0600

    generated [panel] module

---
+++ apps/frontend/modules/panel/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * panel actions.
+ *
+ * @package    anonvpn.com
+ * @subpackage panel
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class panelActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}

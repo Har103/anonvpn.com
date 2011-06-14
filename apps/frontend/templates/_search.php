c7c414e038133281235667727405d61ea6e0a642
Gary Golden <mail@garygolden.me>
2011-06-15 00:09:33 +0600

    search partial; disabled

---
+++ apps/frontend/templates/_search.php
@@ -0,0 +1,9 @@
+<div class="search">
+  <form id="form" name="form" method="post" action="">
+    <span>
+    <input name="q" type="text" class="keywords" id="textfield" maxlength="50" value="Search..." />
+
+    <input name="b" type="image" src="/images/search.gif" class="button" />
+    </span>
+  </form>
+</div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -31,16 +31,7 @@
 						<?php echo $sf_content; ?>
           </div>
           <div class="sidebar">
-          <div class="search">
-            <form id="form" name="form" method="post" action="">
-              <span>
-              <input name="q" type="text" class="keywords" id="textfield" maxlength="50" value="Search..." />
-
-              <input name="b" type="image" src="/images/search.gif" class="button" />
-              </span>
-            </form>
-          </div>
-          <!--/search -->
+						<?php // include_partial('global/search')?>
             <div class="gadget">
               <h2>Sidebar Menu</h2>
               <div class="clr"></div>

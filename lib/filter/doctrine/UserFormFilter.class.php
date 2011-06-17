709cdaafe3d351888a22612f8a40b5c0180d7be4
Gary Golden <mail@garygolden.me>
2011-06-17 19:54:05 +0600

    symfonize radcheck to User

--- lib/filter/doctrine/UserFormFilter.class.php
+++ lib/filter/doctrine/UserFormFilter.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Radcheck filter form.
+ * User filter form.
  *
  * @package    anonvpn.com
  * @subpackage filter
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class RadcheckFormFilter extends BaseRadcheckFormFilter
+class UserFormFilter extends BaseUserFormFilter
 {
   public function configure()
   {

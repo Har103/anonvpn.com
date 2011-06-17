552e1ece45f62ec32e9cc9d543b4f2f5870444ff
Gary Golden <mail@garygolden.me>
2011-06-17 23:36:30 +0600

    [admin] module for [Server]

---
+++ apps/admin/modules/servers/lib/serversGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * servers module helper.
+ *
+ * @package    anonvpn.com
+ * @subpackage servers
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class serversGeneratorHelper extends BaseServersGeneratorHelper
+{
+}
--- apps/admin/templates/_menu.php
+++ apps/admin/templates/_menu.php
@@ -1,6 +1,7 @@
 <?php
 $menu = new ioMenu();
-$menu->addChild('Customers', '@customer');
-$menu->addChild('Pages', '@page');
+$menu->addChild('Servers',			'@server');
+$menu->addChild('Customers',		'@customer');
+$menu->addChild('Pages',				'@page');
 
 echo $menu;

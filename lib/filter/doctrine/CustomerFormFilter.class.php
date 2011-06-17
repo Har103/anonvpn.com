a113c558329fc98dcc231e058d57e8ce12a0870a
Gary Golden <mail@garygolden.me>
2011-06-17 20:31:55 +0600

    rename [User] to [Customer]

--- lib/filter/doctrine/CustomerFormFilter.class.php
+++ lib/filter/doctrine/CustomerFormFilter.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * User filter form.
+ * Customer filter form.
  *
  * @package    anonvpn.com
  * @subpackage filter
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class UserFormFilter extends BaseUserFormFilter
+class CustomerFormFilter extends BaseCustomerFormFilter
 {
   public function configure()
   {
--- lib/form/doctrine/CustomerForm.class.php
+++ lib/form/doctrine/CustomerForm.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * User form.
+ * Customer form.
  *
  * @package    anonvpn.com
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class UserForm extends BaseUserForm
+class CustomerForm extends BaseCustomerForm
 {
   public function configure()
   {

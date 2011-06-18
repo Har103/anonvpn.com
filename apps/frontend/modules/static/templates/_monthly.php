5d8a04d592ab6b8031589018fd07a34c329640e3
Gary Golden <mail@garygolden.me>
2011-06-18 23:41:28 +0600

    static/monthly make use of [Product] (sku: VPNMONTHLY)

--- apps/frontend/modules/static/templates/_monthly.php
+++ apps/frontend/modules/static/templates/_monthly.php
@@ -1,25 +1,43 @@
-<div class="homepage-widget basic-package">
-  <h2>Basic VPN service</h2>
+<?php
+$product = ProductTable::getInstance()->find('VPNMONTHLY');
+?>
+
+<div class="homepage-widget vpnmonthly">
+  <h2>Monthly VPN service</h2>
   <div class="clr"></div>
-  <p>Standard set of features suitable for most users.</p>
-	<ul>
-		<li>Instant activation</li>
-		<li>Web control panel</li>
-		<li>Encrypted VPN using PPTP</li>
-		<li>Access to VPN servers in Europe</li>
-		<li>Unlimited transfer</li>
-		<li>Technical support</li>
-		<li>7-day money back guarantee</li>
-	</ul>
-	<div class="price">$5</div>
-	<div class="button">
+  <ul>
+    <li>Instant automagic activation</li>
+    <li>Unlimited transfer</li>
+    <li>Access to all VPN servers</li>
+    <li>
+      128-bit
+      <?php
+      echo link_to(
+        'MPPE',
+        'http://wikipedia.org/wiki/Microsoft_Point-to-Point_Encryption',
+        array('title' => 'Wikipedia article about MPPE', 'target' => '_blank'))
+      ?>
+      encryption
+    </li>
+    <li>Shared IP</li>
+    <li>24/7 support</li>
+    <?php /*
+    <li>7-day money back guarantee</li>
+    */ ?>
+  </ul>
+  <div class="price">$<?php echo (int)$product->getPrice()?></div>
+  <div class="button">
   	<form action='https://www.2checkout.com/checkout/purchase' method='post'>
-      <input type='hidden' name='sid' value='1507059' />
-      <input type='hidden' name='quantity' value='1' />
-      <input type='hidden' name='product_id' value='1' />
       <?php if ($sf_context->getConfiguration()->getEnvironment() === 'dev') : ?>
       <input type='hidden' name='demo' value='Y' />
       <?php endif; ?>
+  	  <?php /* Merchant ID */ ?>
+      <input type='hidden' name='sid' value='1507059' />
+      <?php /* 2CO assigned ID */ ?>
+      <input type='hidden' name='product_id' value='<?php echo $product->get2coId()?>' />
+      <input type='hidden' name='quantity' value='1' />
+      <?php /* to remove the Continue Shopping button and lock the quantity fields. */ ?>
+      <input type="hidden" name="fixed" value="Y" />
       <input class="button" name='submit' type='submit' value='Purchase' />
   	</form>
 	</div>

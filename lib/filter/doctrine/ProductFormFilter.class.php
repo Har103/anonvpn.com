9c7082b9cbadf215ec7494f6e915a3e35bd1dfdb
Gary Golden <mail@garygolden.me>
2011-06-18 18:42:08 +0600

    rename [Rate] to [Product]

--- lib/filter/doctrine/ProductFormFilter.class.php
+++ lib/filter/doctrine/ProductFormFilter.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Rate filter form.
+ * Product filter form.
  *
  * @package    anonvpn.com
  * @subpackage filter
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class RateFormFilter extends BaseRateFormFilter
+class ProductFormFilter extends BaseProductFormFilter
 {
   public function configure()
   {

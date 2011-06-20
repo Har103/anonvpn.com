57eaca3ceb8844fe794c117c56a786bed3105976
Gary Golden <mail@garygolden.me>
2011-06-21 04:31:49 +0600

    rename [Radip] to [Ip]

--- lib/filter/doctrine/IpFormFilter.class.php
+++ lib/filter/doctrine/IpFormFilter.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Radip filter form.
+ * Ip filter form.
  *
  * @package    anonvpn.com
  * @subpackage filter
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class RadipFormFilter extends BaseRadipFormFilter
+class IpFormFilter extends BaseIpFormFilter
 {
   public function configure()
   {

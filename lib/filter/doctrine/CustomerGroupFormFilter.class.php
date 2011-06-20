6ff4a8f4053c5dfc40ed31aa39ac07c13b9f9c91
Gary Golden <mail@garygolden.me>
2011-06-21 04:14:31 +0600

    [Group] model; related to [Customer] (many-to-many)

---
+++ lib/filter/doctrine/CustomerGroupFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * CustomerGroup filter form.
+ *
+ * @package    anonvpn.com
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class CustomerGroupFormFilter extends BaseCustomerGroupFormFilter
+{
+  public function configure()
+  {
+  }
+}

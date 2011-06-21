a8af13d7bbfaa5b98dd4e82e9318d1a4710e7ad8
Gary Golden <mail@garygolden.me>
2011-06-21 09:36:57 +0600

    [frontend] fix typo in _motd

--- apps/frontend/templates/_motd.php
+++ apps/frontend/templates/_motd.php
@@ -1,6 +1,6 @@
 <h2>Virtual Private Network is...</h2>
 <p>
-	<strong>Technology which allows you to change IP address and pass internet access limitaions.</strong><br />
+	<strong>Technology which allows you to change IP address and pass internet access limits.</strong><br />
 	You will be connected to internet through one of our servers, thus hosts you visit will not see your IP.<br />
 	Connection between you and us is compressed and encrypted, thus your provider will see only random data.<br />
 	This way you can use any resources which may be restricted by your ISP.<br />

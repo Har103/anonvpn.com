e380d44f435c8f6cd1f53cf75f29beed5b4e4231
Gary Golden <mail@garygolden.me>
2011-06-21 10:07:49 +0600

    static/servers content update

--- apps/frontend/modules/static/templates/_servers.php
+++ apps/frontend/modules/static/templates/_servers.php
@@ -1,4 +1,4 @@
 <h2><span>Worldwide servers</span></h2>
-<p>
-We have servers in US and UK.
-</p>
+<p>Out primary VPN server is located in Denver, US.</p>
+<p>Also, we have servers in UK, Germany and Egypt which can be enabled <a href="mailto:support@anonvpn.com?subject=New server request">on demand.</a></p>
+<p>If you need another location, <a href="mailto:support@anonvpn.com?subject=New server request">contact us</a> and we will setup a server for you within 24 hours.</p>

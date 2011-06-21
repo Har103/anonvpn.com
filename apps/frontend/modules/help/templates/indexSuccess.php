7df992595ea067fccbfb9d4fc56e0fb64a3f23ee
Gary Golden <mail@garygolden.me>
2011-06-21 09:49:05 +0600

    [help] initial page

---
+++ apps/frontend/modules/help/templates/indexSuccess.php
@@ -0,0 +1,12 @@
+<div class="help">
+  <h2>Help topics</h2>
+  <p>
+  <?php
+  echo link_to(
+  	'How to configure VPN in Windows XP',
+  	'http://support.microsoft.com/kb/314076',
+    array('target' => '_blank', 'title' => 'Read instructions at Microsoft support center.')
+  )?>
+  </p>
+
+</div>

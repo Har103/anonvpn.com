739117378ff36251e7de4bc5b793d4377ca5d68f
Gary Golden <mail@garygolden.me>
2011-06-25 20:17:28 +0600

    [Hoster] initial model

---
+++ lib/filter/doctrine/HosterFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Hoster filter form.
+ *
+ * @package    anonvpn.com
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class HosterFormFilter extends BaseHosterFormFilter
+{
+  public function configure()
+  {
+  }
+}

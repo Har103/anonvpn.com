0455f6c7fa69e3bce51c3a11e2f9976ac040aa0c
Gary Golden <mail@garygolden.me>
2011-06-28 16:08:11 +0600

    fresh [admin] module for [Hoster]

---
+++ apps/admin/modules/hosters/lib/hostersGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * hosters module helper.
+ *
+ * @package    anonvpn.com
+ * @subpackage hosters
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class hostersGeneratorHelper extends BaseHostersGeneratorHelper
+{
+}

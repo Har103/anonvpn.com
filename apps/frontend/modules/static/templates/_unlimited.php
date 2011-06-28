9ad4ca9ce7beb4ca159ee54a8754dfbf602512de
Gary Golden <mail@garygolden.me>
2011-06-28 23:29:14 +0600

    [frontend] static/unlimited content update

--- apps/frontend/modules/static/templates/_unlimited.php
+++ apps/frontend/modules/static/templates/_unlimited.php
@@ -1,44 +1,21 @@
 <?php
 $product = ProductTable::getInstance()->find('VPNMONTHLY');
+if (!$product) return;
 ?>
-
-<div class="homepage-widget vpnmonthly">
-  <h2>Monthly VPN service</h2>
+<div class="homepage-widget unlimited">
+  <h2>Unlimited VPN</h2>
   <div class="clr"></div>
   <ul class="green-tick">
-    <li>Instant automagic activation</li>
+    <li>Instant activation</li>
+    <li>No software required</li>
+    <li>100MBit connection *</li>
     <li>Unlimited transfer</li>
-    <li>Access to all VPN servers</li>
-    <li>
-      128-bit
-      <?php
-      echo link_to(
-        'MPPE',
-        'http://wikipedia.org/wiki/Microsoft_Point-to-Point_Encryption',
-        array('title' => 'Wikipedia article explaining MPPE', 'target' => '_blank'))
-      ?>
-      encryption
-    </li>
-    <li>Shared IP</li>
-    <li>24/7 support</li>
-    <?php /*
-    <li>7-day money back guarantee</li>
-    */ ?>
+    <li>Access to all <a title="Unmetered VPN servers list" href="<?php echo url_for('@servers_unlimited')?>">servers</a></li>
+    <li>128-bit <a href="http://wikipedia.org/wiki/Microsoft_Point-to-Point_Encryption" title="Wikipedia article explaining MPPE" target="_blank">encryption</a></li>
   </ul>
-  <div class="floatl">
-  	<form action='https://www.2checkout.com/checkout/purchase' method='post'>
-      <?php if ($sf_context->getConfiguration()->getEnvironment() === 'dev') : ?>
-      <input type='hidden' name='demo' value='Y' />
-      <?php endif; ?>
-  	  <?php /* Merchant ID */ ?>
-      <input type='hidden' name='sid' value='1507059' />
-      <?php /* 2CO assigned ID */ ?>
-      <input type='hidden' name='product_id' value='<?php echo $product->get2coId()?>' />
-      <input type='hidden' name='quantity' value='1' />
-      <?php /* to remove the Continue Shopping button and lock the quantity fields. */ ?>
-      <input type="hidden" name="fixed" value="Y" />
-      <input class="blue-gradient" name='submit' type='submit' value='Sign up' />
-    </form>
+  <div class="footnote">* shared. We do not oversell.</div>
+  <div class="offer-tag">
+  	<span class="price">$<?php echo (int)$product->getPrice()?></span>
+    <span class="sign-up"><a title="Proceed to checkout" href="<?php echo url_for('@checkout_unlimited')?>">Sign up</a></span>
   </div>
-  <div class="price">$<?php echo (int)$product->getPrice()?></div>
 </div>

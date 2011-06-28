cfe983500c70aec4b8c9bee3512c4918538590f2
Gary Golden <mail@garygolden.me>
2011-06-28 16:27:48 +0600

    fresh generated [admin] module for [Order]

--- apps/admin/templates/_menu.php
+++ apps/admin/templates/_menu.php
@@ -3,6 +3,7 @@ $menu = new ioMenu();
 $menu->addChild('Hosters',			'@hoster');
 $menu->addChild('Servers',			'@server');
 $menu->addChild('Customers',		'@customer');
+$menu->addChild('Orders',    		'@order');
 $menu->addChild('Pages',				'@page');
 
 echo $menu;

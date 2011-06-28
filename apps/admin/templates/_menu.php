136077bf334ab6c11e75c3d953252b4858991756
Gary Golden <mail@garygolden.me>
2011-06-28 16:10:53 +0600

    menu element for [admin] module [Hoster]

--- apps/admin/templates/_menu.php
+++ apps/admin/templates/_menu.php
@@ -1,5 +1,6 @@
 <?php
 $menu = new ioMenu();
+$menu->addChild('Hosters',			'@hoster');
 $menu->addChild('Servers',			'@server');
 $menu->addChild('Customers',		'@customer');
 $menu->addChild('Pages',				'@page');

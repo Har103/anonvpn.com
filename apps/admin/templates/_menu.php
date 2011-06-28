07e4ec62e4f3a66ae96d70e767777905beaf72a1
Gary Golden <mail@garygolden.me>
2011-06-28 21:22:06 +0600

    fresh [admin] module for [Product]

--- apps/admin/templates/_menu.php
+++ apps/admin/templates/_menu.php
@@ -2,7 +2,8 @@
 $menu = new ioMenu();
 $menu->addChild('Hosters',			'@hoster');
 $menu->addChild('Servers',			'@server');
-$menu->addChild('Customers',		'@customer');
+$menu->addChild('Customers',    '@customer');
+$menu->addChild('Products',  	  '@product');
 $menu->addChild('Orders',    		'@order');
 $menu->addChild('Pages',				'@page');
 
